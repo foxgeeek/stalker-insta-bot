45677f52f6e457edd385726df97dc116b67dbb1b
Leonardo Lacerda <leonardo_lacerda@carrefour.com>
2025-02-04 15:21:40 -0300

    feat: test bot using github actions

--- src/server.js
+++ src/server.js
@@ -9,18 +9,18 @@ const app = express();
 app.use(express.json());
 
 // Valida e checa novas postagens, só passa após fazer o check completo.
-setInterval(async () => {
-  if (isChecking) return; // Evita execuções simultâneas
+// setInterval(async () => {
+//   if (isChecking) return; // Evita execuções simultâneas
 
-  isChecking = true;
-  try {
-    await checkNewPost();
-  } catch (error) {
-    console.error("Erro ao verificar novo post:", error);
-  } finally {
-    isChecking = false;
-  }
-}, CHECK_INTERVAL);
+//   isChecking = true;
+//   try {
+//     await checkNewPost();
+//   } catch (error) {
+//     console.error("Erro ao verificar novo post:", error);
+//   } finally {
+//     isChecking = false;
+//   }
+// }, CHECK_INTERVAL);
 
 app.listen(PORT, async () => {
   console.log(`\n🛜  Servidor rodando na porta ${PORT}`);
